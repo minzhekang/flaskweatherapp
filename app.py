3ff96a220d014b8f1f9e77570fc16c7159272644
Kang Min Zhe <36184648+minzhekang@users.noreply.github.com>
2019-08-05 20:49:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,32 +12,27 @@ def index():
         ip = request.remote_addr
     else:
         ip = request.headers.getlist("X-Forwarded-For")[0]
-    print(ip)
-    print(request.headers.getlist("X-Forwarded-For")[0])
+
+    s = requests.Session()
+    response = s.get("http://ip-api.com/json/{}".format(ip)).json()
+    print(response)
+    country = response["city"]
+    lat = str(response["lat"])
+    long = str(response["lon"])
+    coord = (lat+','+long)
+
+    weather = s.get("https://darksky.net/forecast/{}/ca12".format(coord))
+    weatherhtml= weather.text
+    soup = BeautifulSoup(weatherhtml,'html.parser')
+    currently = soup.select('span.summary.swap')[0].text
+    feelslike = soup.find('span', class_='summary-high-low').find('span').text
+    image = soup.find('span', class_='currently').find('img')['src']
+    image_src = ("https://darksky.net" + image)
     
-    if ip == "127.0.0.1" or "localhost":
-        raise ValueError("localhost detected!")
-    else:
-        s = requests.Session()
-        response = s.get("http://ip-api.com/json/{}".format(ip)).json()
-        print(response)
-        country = response["city"]
-        lat = str(response["lat"])
-        long = str(response["lon"])
-        coord = (lat+','+long)
+    weather_data = {'country': country, 'currently': currently, 'feelslike':feelslike, 'image_src': image_src}
 
-        weather = s.get("https://darksky.net/forecast/{}/ca12".format(coord))
-        weatherhtml= weather.text
-        soup = BeautifulSoup(weatherhtml,'html.parser')
-        currently = soup.select('span.summary.swap')[0].text
-        feelslike = soup.find('span', class_='summary-high-low').find('span').text
-        image = soup.find('span', class_='currently').find('img')['src']
-        image_src = ("https://darksky.net" + image)
-        
-        weather_data = {'country': country, 'currently': currently, 'feelslike':feelslike, 'image_src': image_src}
-
-        print(country)
-        print(lat, long)
+    print(country)
+    print(lat, long)
     return render_template('index.html', weather_data= weather_data)
     
 app.wsgi_app = ProxyFix(app.wsgi_app)
