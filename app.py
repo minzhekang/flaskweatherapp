dd87d3d05d74c56a705bb83452fab869056599e0
Kang Min Zhe <36184648+minzhekang@users.noreply.github.com>
2019-08-05 20:18:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
